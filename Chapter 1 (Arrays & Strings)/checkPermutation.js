98937dd3df4466fe357cba07d1269f2b3fed6721
Gideon Baik <gideonbaik@gmail.com>
2017-11-05 19:54:12 -0800

    Add another implementation for checkPermutation

--- Chapter 1 (Arrays & Strings)/checkPermutation.js
+++ Chapter 1 (Arrays & Strings)/checkPermutation.js
@@ -20,3 +20,45 @@ var checkPermutation0 = (string1, string2) => {
   return sort1 === sort2;
 }
 
+/*
+  Space Complexity: O(n)
+  Time Complexity: O(n)
+*/
+
+var checkPermutation1 = (string1, string2) => {
+  // Declare object
+  var hash = {};
+  
+  // Iterate through string1
+  for (var i = 0; i < string1.length; i++) {
+    // Check if hash has a value, if not set to 0
+    hash[string1[i]] = hash[string1[i]] || 0;
+    // Increment count
+    hash[string1[i]]++;
+  }
+
+  // Iterate through string2
+  for (var i = 0; i < string2.length; i++) {
+    // If letter exist in object
+    if (hash[string2[i]] !== undefined) {
+      // Decrement count
+      hash[string2[i]]--;
+    } else { // Else
+      // Return false (since letter does not exist)
+      return false;
+    }
+  }
+  console.log(hash);
+  // Iterate through object
+  for (var letter in hash) {
+    // If a letter value does not equal 0
+    if (hash[letter] !== 0) {
+      // Return false
+      return false;
+    }
+  }
+
+  //Return true
+  return true;
+}
+
