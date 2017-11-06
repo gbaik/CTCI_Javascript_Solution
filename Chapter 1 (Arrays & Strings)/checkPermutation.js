07ec9dc220c459ecf05f59d20cf93db7a4591ad8
Gideon Baik <gideonbaik@gmail.com>
2017-11-05 19:36:05 -0800

    Cleanup checkPermutation brute force implementation

--- Chapter 1 (Arrays & Strings)/checkPermutation.js
+++ Chapter 1 (Arrays & Strings)/checkPermutation.js
@@ -10,10 +10,13 @@
   Time Complexity: O(n log n)
 */
 
-var checkPermutation = (string1, string2) => {
+var checkPermutation0 = (string1, string2) => {
+  // Sort string1
   var sort1 = string1.split('').sort().join('');
+  // Sort string2
   var sort2 = string2.split('').sort().join('');  
 
+  // Return if sort1 is equal to sort2
   return sort1 === sort2;
 }
 
