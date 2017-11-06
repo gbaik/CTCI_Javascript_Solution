b802c526e53bac9ef1ab277b5006518b80987125
Gideon Baik <gideonbaik@gmail.com>
2017-11-05 20:22:38 -0800

    Fix URLify edge case

--- Chapter 1 (Arrays & Strings)/URLify.js
+++ Chapter 1 (Arrays & Strings)/URLify.js
@@ -6,12 +6,30 @@
   I: String, Number (length of string)
   O: String (spaces replaced with %20)
   C: N/A
-  E: N/A
+  E: If there are no more characters at the end, just cut it off (i.e "Mr John Smith " === "Mr%20John%20Smith")
 
   Space Complexity: O(1)
   Time Complexity: O(n)
 */
 
 // Split on space & Join on %20
-var URLify = (string, length) => (string.split(' ').join('%20'));
+var URLify = (string, length) => {
+  // Split on space 
+  var split = string.split(' ');
+  
+  console.log(split);
+  
+  // Iterate through split string backwards
+  for (var i = split.length - 1; i >= 0; i--) {
+    console.log(split[i]);
+    // If current value is equal to ""
+    if(split[i] === "") {
+      // Remove last item in split array
+      split.pop();
+    }
+  }
+  
+  // Return Join split string on %20
+  return split.join('%20')
+ }
 
