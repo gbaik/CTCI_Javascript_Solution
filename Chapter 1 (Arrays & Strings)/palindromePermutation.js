becf047eeb613d8d67a97458aac4ffac801ac526
Gideon Baik <gideonbaik@gmail.com>
2017-11-06 19:31:23 -0800

    Add palindromePermutation solution

---
+++ Chapter 1 (Arrays & Strings)/palindromePermutation.js
@@ -0,0 +1,47 @@
+/*
+  Given a string, write a function to check if it is a permutation of a palin- drome. 
+  A palindrome is a word or phrase that is the same forwards and backwards. 
+  A permutation is a rearrangement of letters.The palindrome does not need to be limited to just dictionary words.
+
+  I: String
+  O: Boolean Value
+  E: N/A
+  C: N/A
+
+  Space Complexity: O(n)
+  Time Complexity: O(n)
+*/
+
+var palindromePermutation = (string) => {
+  // Set string to lowercase
+  string = string.toLowerCase();
+  // Declare object
+  var hash = {};  
+  // Declare count
+  var count = 0;  
+
+  // Iterate through string
+  for (var i = 0; i < string.length; i++) {  
+    // If the letter is not empty  
+    if (string[i] !== ' ') {
+      // Check if hash has the letter if not set to 0
+      hash[string[i]] = hash[string[i]] || 0;    
+      // Increment count of letter
+      hash[string[i]]++;
+      }
+  }    
+  
+  // Iterate through the hash
+  for (var letter in hash) {    
+    // If object value module 2 equals 1
+    if (hash[letter] % 2) {      
+      // Increment count
+      count++;
+    }
+  }
+
+  // Return if count is greater than or equal to 1
+  return count <= 1;
+}
+
+    
