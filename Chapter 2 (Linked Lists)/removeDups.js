4421a91a413eca9577f15328abff535af9d9cfe9
Gideon Baik <gideonbaik@gmail.com>
2017-11-14 19:17:24 -0800

    Cleanup removeDups

--- Chapter 2 (Linked Lists)/removeDups.js
+++ Chapter 2 (Linked Lists)/removeDups.js
@@ -6,66 +6,37 @@
   C: N/A
   E: N/A
 
-  Space Complexity: 
-  Time Complexity:
+  Space Complexity: O(n)
+  Time Complexity: O(n)
 */
-
-var linkedList = require('./basicImplementation');
 
 var removeDups = (linkedList) => {
   // Declare hash object
   var hash = {};
-  var node1 = linkedList.head;
-  var node2 = node1.next;
+  var current = linkedList.head;
+  var next = current.next;
 
   // Iterate through linked list
-  while(node2) {
+  while(next) {
     if(!hash.length) {
       // Insert first number into hash (to check for duplicates)
-      hash[node1.value] = true;
+      hash[current.value] = true;
     }
-    var value = node2.value;
+    var value = next.value;
     // Check if the value is inside the hash object
     if(hash[value]) {
       // Remove value (skip over the node)
-      node1.next = node2.next;
+      current.next = next.next;
     } else {
       // Insert into hash object      
       hash[value] = true;
       // Move onto the next node
-      node1 = node2;
+      current = next;
     }
     // Move onto next node
-    node2 = node1.next;
+    next = current.next;
   }
 
   // Return linked list
   return linkedList;
 }
-
-
-var test = (list) => {
-  var node = list.head;
-
-  while(node) {
-    console.log(node.value);
-    node = node.next;
-  }
-}
-
-linkedList.addToTail(1);
-linkedList.addToTail(5);
-linkedList.addToTail(5);
-linkedList.addToTail(5);
-linkedList.addToTail(2);
-linkedList.addToTail(7);
-linkedList.addToTail(2);
-linkedList.addToTail(1);
-
-console.log('BEFORE');
-test(linkedList);
-
-removeDups(linkedList);
-
-console.log('AFTER');
-test(linkedList);
