4b03142de0d7faa8562953f46ed1aa413166469c
Gideon Baik <gideonbaik@gmail.com>
2017-11-14 18:09:56 -0800

    Add linkedList basic implementation

--- Chapter 2 (Linked Lists)/basicImplementation.js
+++ Chapter 2 (Linked Lists)/basicImplementation.js
@@ -1,19 +1,45 @@
 var linkedList = function() {
   var list = {}
 
-  list.val = null;
-  list.next = null;  
+  list.head = null;
+  list.tail = null;  
+
+  list.addToTail = function(value) {
+    var newTail = Node(value);
+
+    if (!list.head) {
+      list.head = newTail;
+    }
+
+    if(list.tail) {
+      list.tail.next = newTail;
+    }
+
+    list.tail = newTail;
+  };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return null;
+    }
 
+    var currentHeadValue = list.head.value;
+    list.head = list.head.next;
+    return currentHeadValue;
   };
 
-  list.addToTail = function() {
-    
-  };
+  list.contains = function(target) {
+    var node = list.head;
 
-  list.contains = function() {
+    while(node) {
+      if (node.value === target) {
+        return true;
+      }
 
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
