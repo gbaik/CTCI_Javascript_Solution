eddbf62874ff3522113544dda14eb89443b8ef51
Gideon Baik <gideonbaik@gmail.com>
2017-11-14 17:49:58 -0800

    Add basic outline of linkedList implementation

---
+++ Chapter 2 (Linked Lists)/basicImplementation.js
@@ -0,0 +1,30 @@
+var linkedList = function() {
+  var list = {}
+
+  list.val = null;
+  list.next = null;  
+
+  list.removeHead = function() {
+
+  };
+
+  list.addToTail = function() {
+    
+  };
+
+  list.contains = function() {
+
+  };
+
+  return list;
+};
+
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+}
