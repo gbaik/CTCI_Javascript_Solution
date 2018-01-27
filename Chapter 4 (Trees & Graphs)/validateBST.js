0532d72f3838de266292cebbdda42f233c1bcf52
Gideon Baik <gideonbaik@gmail.com>
2018-01-27 01:00:47 -0800

    Add validateBST solution

---
+++ Chapter 4 (Trees & Graphs)/validateBST.js
@@ -0,0 +1,35 @@
+/*
+  Implement a function to check if a binary tree is a binary search tree.
+
+  I: Root (Binary Tree)
+  O: Boolean value
+  C: N/A
+  E: N/a
+
+  Space Complexity: O(n)
+  Time Complexity: O(log n)
+*/
+
+var validateBST = (root) => {
+  var arr = [];
+
+  function traverse(node) {
+    if (!node) {
+      return;
+    }
+
+    traverse(node.left);
+    arr.push(node.value);
+    traverse(node.right);
+  }
+
+  traverse(root);
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] >= arr[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
